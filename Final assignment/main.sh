d10a0c23de8946de1885524def3eecf4634c8d2a
Tijn Bergmans <t.bergmans@student.tue.nl>
2025-04-17 17:33:57 +0200

    Bug fix

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -3,12 +3,11 @@ wandb login
 python3 train.py \
     --data-dir ./data/cityscapes \
     --batch-size 16 \
-    --epochs 300 \
+    --epochs 180 \
     --lr 1e-4 \
     --num-workers 10 \
     --seed 42 \
     --experiment-id "(Pre) Training Half-res" \
-    --checkpoint-interval 5 \
     --early-stopping-patience 10 \
     --pre-train 0 \
     --load-weight 0\
